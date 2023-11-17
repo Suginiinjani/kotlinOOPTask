2eb9388f952ab45afbbed3630e166fb8a25b3a1a
Injani Sugini <mavratfy97@gmail.com>
2023-11-17 18:29:14 +0700

    Exercise 4 done

--- src/main/kotlin/KotlinSubmission-Morning/exercise4/App.kt
+++ src/main/kotlin/KotlinSubmission-Morning/exercise4/App.kt
@@ -1,10 +1,16 @@
 package id.infinitelearning.KotlinSubmission.exercise4
-
-fun main() {
-
-}
     /**
      * Buatlah contoh code penggunaan try catch untuk menangani suatu kasus sesuai keinginan teman-teman
      *
      */
     // Buat di bawah sini
+    fun main() {
+        try {
+            val userInput = "dsm"
+            val intValue = userInput.toInt()
+            println("Nilai integer: $intValue")
+        } catch (e: NumberFormatException) {
+            println("Error: Gagal mengkonversi ke integer. Input bukan angka.\nInput harus berupa angka!")
+        }
+    }
+
